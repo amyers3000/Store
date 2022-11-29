7bf6237f99fdc2269f3927c810b0c502628fa6cd
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-11-29 14:12:16 -0600

    added Nav links and product description

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 
@@ -11,13 +16,13 @@ function App() {
   const theme = createTheme({
     palette: {
       mode: paletteType,
-      background:{
+      background: {
         default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
     }
   })
-  function handleThemeChange(){
-      setDarkMode(!darkMode)
+  function handleThemeChange() {
+    setDarkMode(!darkMode)
   }
 
 
@@ -26,7 +31,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </Container>
 
     </ThemeProvider>
